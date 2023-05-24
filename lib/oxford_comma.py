3f1a1d1dd09d70c0bce4eaa3dbd452152aeaeed4
Adeyemi Arimoro <arimoro71@gmail.com>
2023-05-23 19:21:44 -0500

    Add solution

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+
+    elif len(items)== 2:
+        first = items[0]
+        second = items[1]
+        return f"{first} and {second}"
+    else:
+        all_but_last = items[0:-1]
+        last = items[-1]
+        new_but_last = ", ".join(all_but_last)
+        return f"{new_but_last}, and {last}"
